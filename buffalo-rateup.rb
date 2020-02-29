4fe5947b42a643b58ed3e7cb8b16f743f378ebde
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-02-29 19:41:35 +0900

    IP address read from ARGV

--- buffalo-rateup.rb
+++ buffalo-rateup.rb
@@ -1,15 +1,20 @@
 # coding: utf-8
+#
+# tested Buffalo AP
+#    WHR-1166DHP3 Version 2.94
+#    WSR-1166DHP3 Version 1.16
+
 
 require 'pp'
 require 'mechanize'
 
-interface = ARGV[0]
+router_ip = ARGV[0]
+interface = ARGV[1]
+password = ARGV[2]
 
-router_ip = "192.168.11.1"
-password = ARGV[1]
+backup_file ="/tmp/buffalo-rateup-#{router_ip}.html"
+backup_dir = "/tmp/BUFFALO-RATEUP-#{router_ip}/"
 
-backup_file ="/tmp/buffalo-rateup.html"
-backup_dir = "/tmp/BUFFALO-RATEUP/"
 html = ""
 if (File.exist?(backup_file) && (DateTime.now.to_time - File.mtime(backup_file)) <= 15)
   File.open(backup_file, "r:UTF-8") do |body|
@@ -18,9 +23,8 @@ if (File.exist?(backup_file) && (DateTime.now.to_time - File.mtime(backup_file))
     end
   end
 else
-  agent = Mechanize.new #{|a| a.log = Logger.new(STDERR) }
+  agent = Mechanize.new
   agent.get("http://"+router_ip+"/login.html")
-  #puts agent.page.body
   sleep(0.05)
   
   agent.page.form_with(:id => "authform") do |form|
@@ -30,7 +34,6 @@ else
   end
   sleep(0.05)
   agent.get("http://"+router_ip+"/packet.html")
-  #puts agent.page.body
   File.write(backup_file, agent.page.body)
   File.write(backup_dir + Time.now.strftime("%y%m%d-%H%M") + ".html", agent.page.body)
   html = agent.page.body.force_encoding("SJIS").encode("UTF-8")
@@ -38,14 +41,24 @@ else
 end
 
 interfaces = { "Internet" =>    "Internet.*",
+               "Internet-ERROR" =>    "Internet.*",
                "LAN" =>         "LAN側有線.*",
+               "LAN-ERROR" =>         "LAN側有線.*",
                "Wi-Fi5GHz" =>   "LAN側無線.*5GHz.*",
-               "Wi-Fi2.4GHz" => "LAN側無線.*2.4GHz.*"}
+               "Wi-Fi5GHz-ERROR" =>   "LAN側無線.*5GHz.*",
+               "Wi-Fi2.4GHz" => "LAN側無線.*2.4GHz.*",
+               "Wi-Fi2.4GHz-ERROR" => "LAN側無線.*2.4GHz.*"}
 
 if (html =~ /<th>#{interfaces[interface]}<\/th>\n\s*<td><div class="DIGIT"><!--\d*-->(\d*)<\/div><\/td>\n\s*<td><div class="DIGIT"><!--\d*-->(\d*)<\/div><\/td>\n\s*<td><div class="DIGIT"><!--\d*-->(\d*)<\/div><\/td>\n\s*<td><div class="DIGIT"><!--\d*-->(\d*)<\/div><\/td>/)
-  puts $1
-#  puts $2
-  puts $3
-#  puts $4
+  send = $1
+  send_error = $2
+  recv = $3
+  recv_error = $4
+  if (interface =~ /ERROR$/)
+    puts send_error
+    puts recv_error
+  else
+    puts send
+    puts recv
+  end
 end
-
